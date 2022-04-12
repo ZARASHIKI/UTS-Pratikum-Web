b01147e4858a63959c14968f7b32b7c7bf6f3f96
ZARASHIKI <Agusyusup69@gmail.com>
2022-04-12 15:27:33 +0800

    Random Background rev

--- Script.js
+++ Script.js
@@ -1,6 +1,4 @@
 let box = document.getElementById('box');
-var color = ['#EDFFEC',"#61E786","#5A5766"]
-var i = 0
 document.onmousemove = (event) => {
     box.style.left = event.clientX - (box.clientWidth/2) + 'px'; 
     box.style.top  = event.clientY - (box.clientHeight/2)+ 'px';
